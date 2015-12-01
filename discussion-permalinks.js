4e6e431cbdc625d1a2f32f67c91ebf99de35c054
Seth Battis <sethbattis@stmarksschool.org>
2015-11-30 19:23:57 -0500

    jQuery-ified

--- discussion-permalinks.js
+++ discussion-permalinks.js
@@ -5,6 +5,6 @@ var canvashack = {
 			if (id !== undefined && id.substr(0, 6) === 'entry-') {
 				$(this).find('.ui-menu').first().append('<li class="ui-menu-item" role="presentation"><a href="#' + id + '"><i class="icon-link"></i> permalink</a></li>');
 			}
-		})
+		});
 	}
-}
+};
